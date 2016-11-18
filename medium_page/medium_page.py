79a4fbd1c9244f3179d8020a83f97bbe679fea38
willdeuschle <wjdeuschle@gmail.com>
2016-11-18 10:33:47 -0500

    still more work to be done

--- medium_page/medium_page.py
+++ medium_page/medium_page.py
@@ -1,40 +1,16 @@
-import sys
 import requests
+import unidecode
+import simplejson
+from pattern.web import plaintext
 from bs4 import BeautifulSoup, NavigableString
-
-invalid_tags = ['b', 'i', 'u', 'strong', 'p', 'em', 'a', 'span', 'img']
-
-def clean_text(tag):
-    if tag.name in invalid_tags:
-        s = ""
-
-        for c in tag.contents:
-            if not isinstance(c, NavigableString):
-                c = clean_text(c)
-            s += unicode(c)
-
-        tag = s
-
-    return tag
 
 
 def medium_page(event, context):
     url = event.get('url', None)
-    data_arr = []
     page = requests.get(url).content
     soup_page = BeautifulSoup(page, 'html.parser')
-    stuff = soup_page.find_all('p')
-    for datum in stuff:
-        try:
-            if "card-description" in  datum.get('class', []):
-                pass
-            else:
-                new_datum = clean_text(datum)
-                new_datum_string = new_datum
-                data_arr.append(new_datum_string)
-        except Exception, e:
-            print "The exception: ", e
-            pass
-
-    article_string = " ".join(data_arr)
-    return(article_string)
+    content = soup_page.find('div', class_='section-content')
+    str_content = str(content)
+    plain_content = plaintext(str_content)
+    decoded_content = unidecode.unidecode(plain_content)
+    return simplejson.dumps(decoded_content.replace('\n', ' '))
