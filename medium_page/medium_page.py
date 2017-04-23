be78f7d13f044268caddb69e1d319f6f3f56c446
willdeuschle <wjdeuschle@gmail.com>
2017-04-23 10:23:04 -0400

    improved performance

--- medium_page/medium_page.py
+++ medium_page/medium_page.py
@@ -1,16 +1,17 @@
 import requests
 import unidecode
-import simplejson
 from pattern.web import plaintext
-from bs4 import BeautifulSoup, NavigableString
+from bs4 import BeautifulSoup
 
 
 def medium_page(event, context):
     url = event.get('url', None)
     page = requests.get(url).content
     soup_page = BeautifulSoup(page, 'html.parser')
-    content = soup_page.find('div', class_='section-content')
-    str_content = str(content)
-    plain_content = plaintext(str_content)
-    decoded_content = unidecode.unidecode(plain_content)
+    # find all the content
+    content = soup_page.find_all('div', class_='section-content')
+    # map over it to parse out the nonsense, convert to unicode
+    content = map(lambda content_subpiece: unidecode.unidecode(plaintext(str(content_subpiece))), content)
+    # join with new lines
+    decoded_content = '\n\n'.join(content)
     return decoded_content
